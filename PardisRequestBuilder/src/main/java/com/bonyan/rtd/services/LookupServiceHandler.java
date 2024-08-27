8c57ac86a1f18b5e8d1570a429d5f46b456c479c
catcry <catcry@live.com>
2024-08-28 02:30:29 +0330

    Pardis Request Body - Dir move

---
+++ PardisRequestBuilder/src/main/java/com/bonyan/rtd/services/LookupServiceHandler.java
@@ -0,0 +1,45 @@
+package com.bonyan.rtd.services;
+
+import com.bonyan.rtd.PardisRequestBuilder;
+import com.comptel.eventlink.core.Nodebase;
+import com.comptel.mc.node.*;
+import com.comptel.mc.node.lookup.*;
+import com.comptel.mc.node.logging.NodeLoggerFactory;
+import com.comptel.mc.node.logging.TxeLogger;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+
+
+import java.util.List;
+
+public class LookupServiceHandler {
+
+    private static final Logger logger = Logger.getLogger(PardisRequestBuilder.class.getName());
+    private static final TxeLogger nodeLogger = NodeLoggerFactory.getNodeLogger(PardisRequestBuilder.class.getCanonicalName());
+    private final LookupTable contentTable;
+
+    public LookupServiceHandler (LookupTable contentTable){
+        this.contentTable = contentTable;
+
+    }
+
+
+    public String doLookup(String action_id, String channel) throws Exception{
+        String messageContent="";
+        try {
+            List<NormalLookupResultItem> result = contentTable.lookup(3, 2, 1, action_id, channel);
+
+            if (result == null || result.isEmpty()) {
+                throw new RuntimeException("Record not found in the lookup table: " + contentTable.getName());
+
+            }
+            messageContent = result.get(0).getReturnValues().get(0);
+
+        } catch (RuntimeException e) {
+            messageContent = "";
+        }
+        return messageContent;
+    }
+}
---
+++ PardisRequestBuilder/src/main/java/com/bonyan/rtd/services/RequestWriter.java
@@ -0,0 +1,8 @@
+package com.bonyan.rtd.services;
+
+import com.comptel.eventlink.core.Nodebase;
+
+public class RequestWriter extends Nodebase {
+
+
+}
